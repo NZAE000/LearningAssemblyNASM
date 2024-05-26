af8dce08de69657aa4220b3d8e63d1a73a28bf2a
NZAE000 <85522764+NZAE000@users.noreply.github.com>
2024-05-26 00:22:46 +0000

    assembly code example with arch32 added.

---
+++ arch32/hello32.asm
@@ -0,0 +1,36 @@
+section .data ;; Datos
+    msg db "hola", 0x0A, 0 ;; "hola\n\0"
+
+section .text ;; Code (read only!!)
+    global main
+    extern printf ;; 'extern' is a adicional directive: printf is a extern global symbol
+
+prn: ;; Any function
+    push ebp
+    mov  ebp, esp
+    
+    ;; After call, load parameters in the stack
+    mov  eax, msg ;; Load msg address to eax register
+    push eax      ;; Can be do 'push' to hand:  sub esp, 4  then  mov [esp], eax  ([esp] is to access the stack at position esp)
+    ;;push msg    ;; Can also be done directly, instead of doing it with a registry
+    call printf
+    pop eax       ;; Can be do 'pop' to hand:   add esp, 4 
+
+    mov esp, ebp
+    pop ebp
+
+    ret
+
+main:
+    ;; ebp, esp = registers
+    push ebp      ;; Store old ebp value(begin address of before stack frame) to begin stack frame
+    mov  ebp, esp ;; Load esp value on ebp (update ebp): ebp store(pointer to) begin stack frame address
+
+    call prn
+
+    mov esp, ebp ;; esp pointer to begin stack frame
+    pop ebp      ;; Restore begin address of before stack frame on ebp
+
+    ;;mov eax, 0 ;; (2bytes)
+    xor eax, eax ;; (1byte)
+    ret ;; return eax value
